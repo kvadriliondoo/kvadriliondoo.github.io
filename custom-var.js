6d173dd5ce644a5ddefe646b3893b2a94ae6f3ed
kvadriliondoo <kvadriliondoo@gmail.com>
2020-07-26 21:20:32 +0200

    Comming soon

--- custom-var.js
+++
@@ -1,3 +0,0 @@
-var GOOGLE_ANALYTICS_ID = "UA-173531258-1"; // example "UA-XXXXXXXX-X"
-var CRISP_WEBSITE_ID = "7a4b6dab-7684-4716-bc62-d031ac2f25a2";    // example "9x5x-25x-4x67-x065-x91xxxxx6x83"
-var FORMSPREE_EMAIL = "kvadriliondoo@gmail.com";     // example bdavidxyz@gmail.com
